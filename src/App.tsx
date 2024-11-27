43170340d6049f38592cb43b5050e3cd27f33cff
Jose Chong <127264125+Jlchong3@users.noreply.github.com>
2024-11-27 18:47:22 -0500

    siguiendo guias

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,86 @@
 import './App.css';
 import Grid from '@mui/material/Grid2';
 import IndicatorWeather from './components/IndicatorWeather';
+import TableWeather from './components/TableWeather';
+import ControlWeather from './components/ControlWeather';
+import LineChartWeather from './components/LineChartWeather';
+ {/* Hooks */ }
+import { useEffect, useState } from 'react';
+interface Indicator {
+    title?: String;
+    subtitle?: String;
+    value?: String;
+}
 
 function App() {
+{/* Variable de estado y función de actualización */}
+    let [indicators, setIndicators] = useState<Indicator[]>([])
 
-  return (
-    <Grid container spacing={5}>
+{/* Hook: useEffect */}
+    useEffect( ()=>{
+        let request = async () => {
+        {/* Request */}
+            let API_KEY = "98fb8ef301729630999f7d4360126e5b"
+            let response = await fetch(`https://api.openweathermap.org/data/2.5/forecast?q=Guayaquil&mode=xml&appid=${API_KEY}`)
+            let savedTextXML = await response.text();
 
-      {/* Indicadores */}
-      <Grid size={{ xs: 12, xl: 3 }}><IndicatorWeather/></Grid>
-      <Grid size={{ xs: 12, xl: 3 }}><IndicatorWeather/></Grid>
-      <Grid size={{ xs: 12, xl: 3 }}><IndicatorWeather/></Grid>
-      <Grid size={{ xs: 12, xl: 3 }}><IndicatorWeather/></Grid>
+        {/* XML Parser */}
+            const parser = new DOMParser();
+            const xml = parser.parseFromString(savedTextXML, "application/xml");
 
-      {/* Tabla */}
-      <Grid size={{ xs: 12, xl: 8 }}>Elemento: Tabla</Grid>
+        {/* Arreglo para agregar los resultados */}
 
-      {/* Gráfico */}
-      <Grid size={{ xs: 12, xl: 4 }}>Elemento: Gráfico 1</Grid>
+             let dataToIndicators : Indicator[] = new Array<Indicator>();
 
-    </Grid>
-  )
+             {/*
+                 Análisis, extracción y almacenamiento del contenido del XML
+                 en el arreglo de resultados
+             */}
+
+             let name = xml.getElementsByTagName("name")[0].innerHTML || ""
+             dataToIndicators.push({"title":"Location", "subtitle": "City", "value": name})
+
+             let location = xml.getElementsByTagName("location")[1]
+
+             let latitude = location.getAttribute("latitude") || ""
+             dataToIndicators.push({ "title": "Location", "subtitle": "Latitude", "value": latitude })
+
+             let longitude = location.getAttribute("longitude") || ""
+             dataToIndicators.push({ "title": "Location", "subtitle": "Longitude", "value": longitude })
+
+             let altitude = location.getAttribute("altitude") || ""
+             dataToIndicators.push({ "title": "Location", "subtitle": "Altitude", "value": altitude })
+
+        {/* Modificación de la variable de estado mediante la función de actualización */}
+             setIndicators( dataToIndicators )
+
+        }
+
+        request();
+    }, [] )
+
+    return (
+        <Grid container spacing={5}>
+
+            {/* Indicadores */}
+            {/* <Grid size=> ... </Grid> */}
+
+            {
+                indicators
+                .map(
+                    (indicator, idx) => (
+                        <Grid key={idx} size={{ xs: 12, xl: 3 }}>
+                            <IndicatorWeather
+                                title={indicator["title"]}
+                                subtitle={indicator["subtitle"]}
+                                value={indicator["value"]} />
+                        </Grid>
+                    )
+                )
+            }
+
+        </Grid>
+    )
 }
 
 export default App
--- src/components/IndicatorWeather.tsx
+++ src/components/IndicatorWeather.tsx
@@ -1,7 +1,31 @@
-export default function IndicatorWeather(){
+import Typography from '@mui/material/Typography';
+import Paper from '@mui/material/Paper';
+
+interface Indicator {
+    title?: String;
+    subtitle?: String;
+    value?: String;
+}
+export default function IndicatorWeather(config: Indicator){
     return (
-        <>
-            Component IndicatorWeather
-        </>
+        <Paper
+            sx={{
+                p: 2,
+                display: 'flex',
+                flexDirection: 'column'
+            }}
+        >
+            <Typography component="h2" variant="h6"
+                color="primary" gutterBottom>
+                {config.title}
+            </Typography>
+            <Typography component="p" variant="h4">
+                {config.value.toString()}
+            </Typography>
+            <Typography color="text.secondary" sx={{ flex: 1 }}>
+                {config.subtitle}
+            </Typography>
+        </Paper>
     )
 }
+
